e24c7f573d3a34d32aebb5ac533c9985e36838df
KramYengir <kramyengir@gmail.com>
2023-12-14 14:49:11 +0000

    changed img src paths

--- src/components/cardInfo.js
+++ src/components/cardInfo.js
@@ -1,62 +1,62 @@
 const cards = [
   {
     id: 0,
-    src: "../public/images/ozzy-0.jpg",
+    src: "./images/ozzy-0.jpg",
     clicked: false,
   },
   {
     id: 1,
-    src: "../public/images/ozzy-1.jpg",
+    src: "./images/ozzy-1.jpg",
     clicked: false,
   },
   {
     id: 2,
-    src: "../public/images/ozzy-2.jpg",
+    src: "./images/ozzy-2.jpg",
     clicked: false,
   },
   {
     id: 3,
-    src: "../public/images/ozzy-3.jpg",
+    src: "./images/ozzy-3.jpg",
     clicked: false,
   },
   {
     id: 4,
-    src: "../public/images/ozzy-4.jpeg",
+    src: "./images/ozzy-4.jpeg",
     clicked: false,
   },
   {
     id: 5,
-    src: "../public/images/ozzy-5.jpg",
+    src: "./images/ozzy-5.jpg",
     clicked: false,
   },
   {
     id: 6,
-    src: "../public/images/ozzy-6.jpg",
+    src: "./images/ozzy-6.jpg",
     clicked: false,
   },
   {
     id: 7,
-    src: "../public/images/ozzy-7.jpg",
+    src: "./images/ozzy-7.jpg",
     clicked: false,
   },
   {
     id: 8,
-    src: "../public/images/ozzy-8.jpg",
+    src: "./images/ozzy-8.jpg",
     clicked: false,
   },
   {
     id: 9,
-    src: "../public/images/ozzy-9.jpg",
+    src: "./images/ozzy-9.jpg",
     clicked: false,
   },
   {
     id: 10,
-    src: "../public/images/ozzy-10.jpeg",
+    src: "./images/ozzy-10.jpeg",
     clicked: false,
   },
   {
     id: 11,
-    src: "../public/images/ozzy-11.jpg",
+    src: "./images/ozzy-11.jpg",
     clicked: false,
   },
 ];
